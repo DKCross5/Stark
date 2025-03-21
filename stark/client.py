e24f2314c0d0bb6d773822f93715814c9a290cbf
Andy Le <170907991+DKCross5@users.noreply.github.com>
2025-03-21 00:46:44 -0700

    Added loop function to repeat request

--- stark/client.py
+++ stark/client.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# import websockets
+import websockets
 from websockets.sync.client import connect
 
 def hello():
@@ -9,10 +9,14 @@ def hello():
         name = input("What's your name? ")
 
         websocket.send(name)
-        print(f">>> {"name"}")
+        print(f">>> {name}")
 
         greeting = websocket.recv()
         print(f"<<< {greeting}")
 
 if __name__ == "__main__":
     hello()
+
+# if __name__ == "__main__":
+#     for _ in range(5):  # Loop to run the function 5 times
+#         hello()
